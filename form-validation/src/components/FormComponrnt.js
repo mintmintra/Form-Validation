44659a78c812eef74709c821927b5c99058a3d70
mintmintra <mintracss019@gmail.com>
2022-01-30 00:11:51 +0700

    error message

--- form-validation/src/components/FormComponrnt.js
+++ form-validation/src/components/FormComponrnt.js
@@ -6,6 +6,12 @@ const FormComponent = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [repassword,setRePassword] = useState('')
+
+    const [errorUserName, setErrorUserName] = useState('กรุณาป้อนชื่อผู้ใช้จำนวน 8 ตัวอักษร')
+    const [errorEmail, setErrorEmail] = useState('รูปแบบอีเมลไม่ถูกต้อง')
+    const [errorPassword, setErrorPassword] = useState('รหัสผ่านต้องมีจำนวน 8 ตัวอักษร')
+    const [errorRePassword,setErrorRePassword] = useState('รหัสผ่านไม่ตรงกัน')
+
     return (
         <div className="container">
             <form className="form">
@@ -13,22 +19,22 @@ const FormComponent = () => {
                 <div className="form-control">
                     <label>ชื่อผู้ใช้</label>
                     <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)} />
-                    <small>error message</small>
+                    <small>{errorUserName}</small>
                 </div>
                 <div className="form-control">
                     <label>อีเมล</label>
                     <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} />
-                    <small>error message</small>
+                    <small>{errorEmail}</small>
                 </div>
                 <div className="form-control">
                     <label>รหัสผ่าน</label>
                     <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)} />
-                    <small>error message</small>
+                    <small>{errorPassword}</small>
                 </div>
                 <div className="form-control">
                     <label>ยืนยันรหัสผ่าน</label>
                     <input type="text" value={repassword} onChange={(e)=>setRePassword(e.target.value)}/>
-                    <small>error message</small> 
+                    <small>{errorRePassword}</small> 
                 </div>
                 <button type="submit">ลงทะเบียน</button>
             </form>
